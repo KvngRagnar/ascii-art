84725af8519c8dc2d6326288882f0eda6150e47c
Emaikwu Ngbede <emaikwungbede@gmail.com>
2026-06-16 14:24:30 +0100

    Update render.go

--- render.go
+++ render.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func renderLine(text string, lines []string) {
+
+	for row := 0; row < 8; row++ {
+
+		for _, char := range text {
+			ascii := int(char)
+			index := ascii - 32
+			start := index * 8
+
+			fmt.Println(lines[start+row])
+
+		}
+		fmt.Println()
+	}
+}
